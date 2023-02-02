6494c0041fde94b73f93289aed81cd85b2b123df
Evgeniy Dyachkin <evgeniy.dyachkin@rt.ru>
2023-02-02 16:41:07 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import App from "./App";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
+import "./styles/index.scss";
+
+render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext, Theme } from "./ThemeContext";
+
+const ThemeProvider: FC = ({ children }) => {
+  const defaultTheme =
+    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(() => {
+    return {
+      theme,
+      setTheme,
+    };
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
